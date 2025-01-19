a9620688de96b21fc12bc088e09981c41e55f4ed
MircaSalvatorul <mateimircea919@gmail.com>
2025-01-19 17:20:36 +0200

    Dice Roll animation + Two Dice!

--- dicerollapp.py
+++ dicerollapp.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import random
 import os
+import time
 
 
 class DiceRollApp:
@@ -41,40 +42,54 @@ class DiceRollApp:
 
         self.player2_history = []
 
-        # Cadru pentru zar
+        # Cadru pentru zaruri
         self.dice_frame = tk.Frame(root)
         self.dice_frame.pack(pady=20)
 
-        self.dice_image_label = tk.Label(self.dice_frame)
-        self.dice_image_label.pack()
+        self.dice_image_label1 = tk.Label(self.dice_frame)
+        self.dice_image_label1.pack(side=tk.TOP, padx=10)
+
+        self.dice_image_label2 = tk.Label(self.dice_frame)
+        self.dice_image_label2.pack(side=tk.BOTTOM, padx=10)
 
         # Încarcare imagini zaruri
         self.dice_images = [
-            ImageTk.PhotoImage(Image.open(os.path.join("images", f"dice_{i}.png")).resize((300,300))) for i in range(1, 7)
+            ImageTk.PhotoImage(Image.open(os.path.join("images", f"dice_{i}.png")).resize((150, 150))) for i in range(1, 7)
         ]
 
         self.dice_images_small = [
             ImageTk.PhotoImage(Image.open(os.path.join("images", f"dice_{i}.png")).resize((30, 30))) for i in range(1, 7)
         ]
 
-
     def roll_dice(self, player):
-        dice_result = random.randint(1, 6)
-        self.dice_image_label.config(image=self.dice_images[dice_result - 1])
+
+        for _ in range(20):  #20 de run-uri, 0.1 sec per iteratie
+            dice_result1 = random.randint(1, 6)
+            dice_result2 = random.randint(1, 6)
+            self.dice_image_label1.config(image=self.dice_images[dice_result1 - 1])
+            self.dice_image_label2.config(image=self.dice_images[dice_result2 - 1])
+            self.root.update()
+            time.sleep(0.1)
 
         if player == "Jucător 1":
-            self.update_history(self.player1_history, self.player1_history_frame, dice_result)
+            self.update_history(self.player1_history, self.player1_history_frame, dice_result1, dice_result2)
         elif player == "Jucător 2":
-            self.update_history(self.player2_history, self.player2_history_frame, dice_result)
+            self.update_history(self.player2_history, self.player2_history_frame, dice_result1, dice_result2)
 
-    def update_history(self, history, history_frame, dice_result):
-        history.append(dice_result)
+    def update_history(self, history, history_frame, dice_result1, dice_result2):
+        history.append((dice_result1, dice_result2))
         if len(history) > 5:
             history.pop(0)
 
         for widget in history_frame.winfo_children():
             widget.destroy()
 
-        for roll in history:
-            img_label = tk.Label(history_frame, image=self.dice_images_small[roll - 1])
-            img_label.pack(side=tk.TOP, pady=2)
+        for roll1, roll2 in history:
+            frame = tk.Frame(history_frame)
+            frame.pack(pady=2)
+
+            img_label1 = tk.Label(frame, image=self.dice_images_small[roll1 - 1])
+            img_label1.pack(side=tk.LEFT, padx=2)
+
+            img_label2 = tk.Label(frame, image=self.dice_images_small[roll2 - 1])
+            img_label2.pack(side=tk.RIGHT, padx=2)
